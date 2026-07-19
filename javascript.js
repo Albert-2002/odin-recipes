2d05b72537b67f5be7ef6aabac65fb54f3731f55
Albert Gandhi <albertag2002@gmail.com>
2026-07-19 23:55:28 +0530

    Basic Javascript

---
+++ javascript.js
@@ -0,0 +1,22 @@
+let message = "Hello, world!";
+
+console.log(message);
+
+console.log("Reassignement of message variable");
+
+message = "Hello, Odin!";
+
+console.log(message);
+
+console.log("Basic arithmetic operations");
+
+let a = 10;
+let b = 5;
+
+console.log("a =", a);
+console.log("b =", b);
+
+console.log("a + b =", a + b);
+console.log("a - b =", a - b);
+console.log("a * b =", a * b);
+console.log("a / b =", a / b);
