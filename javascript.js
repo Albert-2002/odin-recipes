b271f81d8df952407062385246d9378e2d121ca8
Albert Gandhi <albertag2002@gmail.com>
2026-07-20 22:51:29 +0530

    Basic JavaScript - 2

--- javascript.js
+++ javascript.js
@@ -8,6 +8,8 @@ message = "Hello, Odin!";
 
 console.log(message);
 
+console.log("------------------------------");
+
 console.log("Basic arithmetic operations");
 
 let a = 10;
@@ -20,3 +22,33 @@ console.log("a + b =", a + b);
 console.log("a - b =", a - b);
 console.log("a * b =", a * b);
 console.log("a / b =", a / b);
+
+console.log("------------------------------");
+
+let bigint = 123n;
+
+console.log("BigInt value:", bigint);
+console.log("Type of BigInt:", typeof bigint);
+
+console.log("------------------------------");
+
+const name = "Sam";
+const age = 25;
+console.log(`My name is ${name} and I am ${age} years old.`);
+// Multi-line strings too:
+const msg = `Line one
+Line two`;
+console.log(msg);
+
+console.log("------------------------------");
+
+console.log("Function to return favorite animal");
+
+function favoriteAnimal(animal) {
+  return animal + " is my favorite animal!";
+}
+
+const animal_msg = favoriteAnimal("Goat");
+console.log(animal_msg);
+
+console.log("------------------------------");
